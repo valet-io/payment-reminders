fabae8313aff17cd6040b3681a73019fa3506b11
Ben Drucker <bvdrucker@gmail.com>
2014-09-25 11:04:20 -0400

    Add note to log

--- run.js
+++ run.js
@@ -1,5 +1,8 @@
 var messages = require('./src');
 
 module.exports = messages.extract()
+  .tap(function (pledges) {
+    console.log('Retrieved %d unpaid pledges', pledges.length);
+  })
   .then(messages.transform)
   .done(messages.load);
