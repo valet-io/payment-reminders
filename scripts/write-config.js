b26583bed6060d39b1c42c407a297efcda6d8df9
Ben Drucker <bvdrucker@gmail.com>
2014-09-26 08:09:06 -0400

    Write bitly env variables

--- scripts/write-config.js
+++ scripts/write-config.js
@@ -8,5 +8,9 @@ fs.writeFile('config.json', JSON.stringify({
   iron: {
     project_id: process.env.IRON_PROJECT_ID,
     token: process.env.IRON_TOKEN
+  },
+  bitly: {
+    username: process.env.BITLY_USERNAME,
+    api_key: process.env.BITLY_API_KEY
   }
 }));
